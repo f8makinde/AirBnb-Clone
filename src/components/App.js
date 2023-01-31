e18c6b23a2056d95ce595b69d3e84f2134b596ae
Faith Makinde <faithmakinde0@gmail.com>
2023-01-31 20:42:47 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Nav from '../components/Nav.js'
+export default function App(){
+    return(
+        <div>
+            <Nav />
+        </div>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import navLogo from "../images/airbnb 1.png"
+export default function Nav(){
+    return (
+        <nav>
+          <img src={navLogo} alt="" />
+        </nav>
+     
+    )
+}
